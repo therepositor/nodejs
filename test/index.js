d05759c53d9f6f91edbdca92eec5e5c9c048b43a
Joseph <joseph.oguntebi@yahoo.com>
2022-01-21 19:27:52 +0100

    addingUnitTest

--- test/index.js
+++ test/index.js
@@ -3,37 +3,16 @@
 *
 */
 
-// Dependencies
-const helpers = require('./../lib/helpers');
-const assert = require('assert');
-
 // Application logic for the test runner
 _app = {};
 
 
 // Container for the tests.
-_app.tests = {
-    'unit': {}
-}
+_app.tests = {};
 
-// Assert that the getANumber function is returning a number 
-_app.tests.unit['helpers.getANumber should return a number'] = (done) => {
-    const val = helpers.getANumber();
-    assert.equal(typeof(val),'number');
-    done();
-}
-// Assert that the getANumber function is returning 1 
-_app.tests.unit['helpers.getANumber should return 1'] = (done) => {
-    const val = helpers.getANumber();
-    assert.equal(val,1);
-    done();
-}
-// Assert that the getANumber function is returning 2 
-_app.tests.unit['helpers.getANumber should return 2'] = (done) => {
-    const val = helpers.getANumber();
-    assert.equal(val,2);
-    done();
-}
+// Add on the unit tests
+_app.tests.unit = require('./unit')
+
 
 // count all test
 _app.countTests = () => {
